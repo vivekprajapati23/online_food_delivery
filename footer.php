2f70b3fdc7c3e8a3123ad4a5cc35aed1c9dc1a49
vivekprajapati23 <prajapativivekk1234@gmail.com>
2025-11-07 18:42:39 +0530

    footer,logincheck,menu

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<div class="footer">
+        <div class="wraper">
+            <p class="text-center">All right reserved ,For Food House</p>
+        </div>
+        </div>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,25 @@
+<?php 
+
+include('../config/constants.php');
+include('login-check.php');
+
+?>
+
+<html>
+    <head>
+        <title>Home -Food page</title>
+        <link rel="stylesheet" href="../css/project.css">
+    </head>
+    <body>
+        <div class="menu text-center">
+            <div class="wraper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-food.php">Food</a></li>
+                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul> 
+            </div>
+        </div>
